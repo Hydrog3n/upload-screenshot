d0741c8c7551a4ec91180f3e86dc0a5644775d76
Magicalex <magicalex@mondedie.fr>
2016-10-30 20:21:10 +0100

    chore(lutim): improve url image for lutim

--- services/chevereto.js
+++ services/chevereto.js
@@ -3,28 +3,28 @@ var fs        = require('fs');
 var notifier  = require('node-notifier');
 
 function Chevereto(service) {
-	this.service = service;
-};
+  this.service = service;
+}
 
-Chevereto.prototype.upload = function(path, callback) { 
+Chevereto.prototype.upload = function(path, callback) {
   var formData = {
     upload: fs.createReadStream(path)
   };
-  
+
   var form = {
     key: this.service.key ? this.service.key : undefined
   };
-  
+
   request.post(this.service.urlapi, {formData: formData, form: form}, function (err, res, body) {
     if (err) {
       notifier.notify({
         'title': 'Error !',
         'message': err
       });
-  
+
       return console.error('upload failed:', err);
     }
-    
+
     var response = JSON.parse(body);
 
     if (response.status_code !== 200) {
--- services/lutim.js
+++ services/lutim.js
@@ -3,31 +3,31 @@ var fs        = require('fs');
 var notifier  = require('node-notifier');
 
 function Lutim(service) {
-	this.service = service;
-};
+  this.service = service;
+}
 
-Lutim.prototype.upload = function(path, callback) { 
- 
+Lutim.prototype.upload = function(path, callback) {
+
   var nameFileArray = path.split('/');
-  var nameFile = nameFileArray[nameFileArray.length - 1]
+  var nameFile = nameFileArray[nameFileArray.length - 1];
   var url = this.service.url;
-  
+
   var formData = {
     file: fs.createReadStream(path),
     filename: nameFile,
     format: 'json'
   };
-  
+
   request.post({url: url, formData: formData}, function (err, res, body) {
     if (err) {
       notifier.notify({
         'title': 'Error !',
         'message': err
       });
-  
+
       return console.error('upload failed:', err);
     }
-    
+
     var response = JSON.parse(body);
 
     if (response.success !== true) {
@@ -37,7 +37,8 @@ Lutim.prototype.upload = function(path, callback) {
       });
     }
 
-    var urlShort = url + response.msg.short;
+    var ext = response.msg.filename.split('.').pop();
+    var urlShort = url + response.msg.short + '.' + ext;
     callback(urlShort);
   });
 };
